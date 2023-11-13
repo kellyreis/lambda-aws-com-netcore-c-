dc0b2193a032cb2f3bddc8dd20cebe66e5f26309
kellyreis <kelly.fernanda.reis94@gmail.com>
2023-11-13 20:33:38 -0300

    criando chamada SDK

---
+++ Lambda-usandoSDK/Program.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.Lambda;
+using Amazon.Lambda.Model;
+
+AmazonLambdaClient client = new AmazonLambdaClient();
+
+const string input = "{\"Nome\":\"Hello World!!\"}";
+
+var request = new InvokeRequest()
+{
+    FunctionName = "Lambda",
+    Payload = input,
+    InvocationType = InvocationType.Event
+};
+
+var response = client.InvokeAsync(request).Result;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Result => {new StreamReader(response.Payload).ReadToEnd()}");
